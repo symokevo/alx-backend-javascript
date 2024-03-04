4906111545abd107173cad63efb8a5214c92082a
Simon Rabuogi <symokevo@gmail.com>
2024-03-04 19:19:42 +0300

    Project folder preparation

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * A message is displayed on STDOUT
+ * @param {string} msg is displayed
+ */
+
+const displayMessage = (string) => {
+     console.log(string);
+};
+
+module.exports = displayMessage;
